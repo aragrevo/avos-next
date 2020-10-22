df5332dbd2807b65031fcfc786b346eb1f7a5dd1
aragrevo <eduver_san@hotmail.com>
2020-10-22 16:00:53 -0500

    Hasta seccion 4

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
-  const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>()
+  const {
+    query: { id },
+  } = useRouter()
+
+  useEffect(() => {
+    id &&
+      window
+        .fetch(`/api/avo/${id}`)
+        .then((response) => response.json())
+        .then((data) => setProduct(data))
+        .catch((error) => console.error)
+  }, [id])
 
   return (
     <section>
-      <h1>Página producto: {query.id}</h1>
+      <h1>Página producto: {id}</h1>
+      <h3>{product?.name}</h3>
     </section>
   )
 }
